6a93f28da05a6cc080eebf2bce02f50fd19f088d
Upendra Kumar <46861757+upendra25312@users.noreply.github.com>
2020-10-14 22:29:37 +0530

    Delete vnet.tf

--- vnet.tf
+++
@@ -1,5 +0,0 @@
-module "network" {
-  source  = "app.terraform.io/UPENDRA_KUMAR-training/network/azurerm"
-  version = "3.0.1"
-  resource_group_name ="azurerm_resource_group.myresourcegroup.name"
-}
